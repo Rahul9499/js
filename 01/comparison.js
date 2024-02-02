dc6c8c412de0f847cdf181d66405d5877dbbb8a0
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-02 08:35:55 +0000

    comparison opr eg

---
+++ 01/comparison.js
@@ -0,0 +1,9 @@
+let str1 = 2;
+let str2 = 4;
+console.log(str1 + str2);
+
+console.log(2 > 1);
+console.log("2" > 1);
+console.log(2 > "1");
+
+//there are some confusion regarding comparison operator.So,we can use it wisely.
