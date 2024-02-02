7953f766bfacc6a98ce820f1efc08ba96409133e
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-02 11:41:56 +0000

    date eg

---
+++ 01/06.date.js
@@ -0,0 +1,16 @@
+// Date
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+let myCreatedDate = new Date("02-02-2024");
+// 
+
+let myTimeStamp = Date.now();
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
