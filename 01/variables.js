cd6ebfef504e50ea3e6976a8a85b625f605f00e1
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-02 06:36:01 +0000

    var eg

---
+++ 01/variables.js
@@ -0,0 +1,11 @@
+const accountId = 15565
+let accountEmail = "rahul@gmail.com"
+var accountPassword = "1234"
+accountCity = "Delhi"
+
+// accountId = 2 //not allowed
+accountEmail = "Rajesh@gmail.com"
+let accountState;
+
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountState]);
