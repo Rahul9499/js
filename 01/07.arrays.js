8e90bb273d51c72900cead03738b56ac35ee2ded
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-02 12:09:20 +0000

    basic array

---
+++ 01/07.arrays.js
@@ -0,0 +1,18 @@
+//Arrays
+
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeros = ["Hanumaan", "Shaktimaan"];
+
+// const myArr2 = new Array(1,2,3,4);
+// console.log(myArr[1]);
+
+//Array Methods
+
+// myArr.push(6);
+// myArr.pop();
+
+// myArr.unshift(5);
+// myArr.shift()
+
+console.log(myArr.includes(7));
+// console.log(myArr);
