e5374784142267f3e6d4af8601b58f6525f6b97f
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-02 13:36:28 +0000

    object eg

---
+++ 01/09.object.js
@@ -0,0 +1,36 @@
+//Object
+//Object literals
+
+const mySym = Symbol["key1"]
+
+const jsUser = {
+    name: "Rahul",
+    "full name":"Rahul Anand",
+    [mySym]: "mykey1",
+    age: 26,
+    location: "Delhi",
+    email: "rahul@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["tuesday", "friday"]
+}
+
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser["full name"]);
+// console.log(jsUser[mySym]);
+
+jsUser.email = "rahul@chatgpt.com"
+//Object.freeze(jsUser)
+jsUser.email = "rahul@amazon.com"
+console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("Hello js user");
+}
+
+jsUser.greeting2 = function(){
+    console.log(`Hello js user,${this.name}`);
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greeting2());
