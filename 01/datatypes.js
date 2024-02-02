ba8fcdcb260a8086b02ada99793bc2846e6dcc88
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-02 07:23:35 +0000

    datatypes eg

---
+++ 01/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //test all js as newer version
+
+// alert(3+3);   //we are using node js not browser
+
+let name = "Rahul";
+let age = 26;
+let isLoggedIn = false;
+let state;
+
+//number = 2 to power 53
+//bigint
+//string =""
+//boolean = true/false
+//null = standalone value
+//undefined
+//symbol
+
+//object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null); //obj
