7ee6f6127a94c264a0f9b21d5f903a0e23098606
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-02 12:20:18 +0000

    array method eg

--- 01/07.arrays.js
+++ 01/07.arrays.js
@@ -14,5 +14,22 @@ const myHeros = ["Hanumaan", "Shaktimaan"];
 // myArr.unshift(5);
 // myArr.shift()
 
-console.log(myArr.includes(7));
-// console.log(myArr);
+// console.log(myArr.includes(7));
+//  console.log(myArr.indexOf(0));
+
+//  let number = myArr.join();
+//  console.log(number);
+
+//slice & splice method
+console.log("A",myArr);
+
+const n1 =myArr.slice(1,3);
+
+console.log(n1);
+console.log("B",myArr);
+
+const n2 = myArr.splice(1,3);
+console.log(n2);
+console.log("c",myArr);
+
+
