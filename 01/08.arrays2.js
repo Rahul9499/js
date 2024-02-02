178c60b62186ddcf4f6a83cbea6de675e92b3032
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-02 12:49:46 +0000

    array method eg

---
+++ 01/08.arrays2.js
@@ -0,0 +1,20 @@
+// const mh = ["thor", "ironman", "spiderman"];
+// const dc = ["superman", "flash", "batman"];
+
+// mh.push(dc);
+// console.log(mh);
+
+// const arr = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+
+// const arr1 = arr.flat(Infinity);
+// console.log(arr1);
+
+// console.log(Array.isArray("Rahul"));
+// console.log(Array.from("Rahul"));
+// console.log(Array.from({name: "Rahul"}));
+
+let n1 = 100
+let n2 = 200
+let n3 = 300
+console.log(Array.of(n1,n2,n3));
+
