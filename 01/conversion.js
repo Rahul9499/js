19055fd326102ffb9cd1feec82aa01bf9ba87a6a
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-02 08:00:11 +0000

    conversion of datatypes

---
+++ 01/conversion.js
@@ -0,0 +1,10 @@
+// let score = 33;
+// console.log(typeof score);
+
+//"33"=> 33
+//"33abc"=> NaN
+// true => 1 ; false => 0;
+
+let somenumber = 55
+let stringNumber = String(somenumber);
+console.log(typeof stringNumber);
