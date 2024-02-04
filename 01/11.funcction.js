539a85858dedf23a3c1165187ff34223d032c941
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-04 10:16:54 +0000

    updated

--- 01/11.funcction.js
+++ 01/11.funcction.js
@@ -1,8 +1,24 @@
-function myName(){
-    console.log("R");
-    console.log("A");
-    console.log("H");
-    console.log("U");
-    console.log("L");
+// function myName(){
+//     console.log("R");
+//     console.log("A");
+//     console.log("H");
+//     console.log("U");
+//     console.log("L");
+// }
+// myName();
+
+// function add(num1, num2){
+//     let result = num1 + num2;
+//     return result;
+// }
+// const result = add(3,7)
+// console.log("Result: ", result);
+
+function loginUser(username = "rahul"){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
 }
-myName();
+console.log(loginUser("anand"));
