42b3870dffe83fa10797e1f4f0bef168c8e9c037
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-05 11:57:19 +0000

    execution eg

---
+++ 01/16.jsexecutioncd.js
@@ -0,0 +1,13 @@
+//global execution
+//functional execution
+//memory phase
+//execution phase
+
+let val1 = 10
+let val2 = 5
+function add(num1, num2){
+    let total = num1 + num2
+    return total
+}
+let result1 = add(val1, val2)
+let result2 = add(2,3)
