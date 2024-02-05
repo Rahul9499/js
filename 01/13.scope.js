d0bb72d72716be58c2658a635487f24c0bf3e806
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-05 09:19:30 +0000

    hoisting

--- 01/13.scope.js
+++ 01/13.scope.js
@@ -1,11 +1,25 @@
 
- let a = 450 
+//  let a = 450 
 
-if(true){
-    let a = 10
-    const b = 20
-    console.log("Inner: ", a);
-}
+// if(true){
+//     let a = 10
+//     const b = 20
+//     console.log("Inner: ", a);
+// }
  
- console.log(a);
+//  console.log(a);
 // console.log(b);
+
+
+function one(){
+    const username = "rahul"
+
+    function two(){
+        const web = "Git"
+       console.log(username);
+    }
+    //console.log(web);
+
+two()
+}
+one()
