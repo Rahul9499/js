049bca1d3314dfa17c224049f8d36f64c6b09532
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-02-05 13:05:48 +0000

    eg 2

--- 01/17.controlflow.js
+++ 01/17.controlflow.js
@@ -1,12 +1,47 @@
 //if
 
-const temp = 45
+// const temp = 45
 
-if(temp === 40 ){
-    console.log("less than 50");
-} else {
-    console.log("temperature is greater than 40");
+// if(temp === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 40");
+// }
+
+// console.log("Executed");
+
+// const balance = 1500;
+
+// if(balance < 500 ){
+//     console.log("less than 500");
+// } else if(balance < 750){
+// console.log("less than 750");
+// } else if (balance < 900 ) {
+//     console.log("less than 900");
+// } else {
+//     console.log("greater than 900");
+// }
+
+
+const month = 4
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    case 5:
+        console.log("may");
+        break;
+
+    default:
+        console.log("default matched");
+        break;
 }
-
-console.log("Executed");
-
