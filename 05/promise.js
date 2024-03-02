90fca49074f56cb311a16c04971cdf460e2cac78
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-03-02 05:42:02 +0000

    fetch basic eg

--- 05/promise.js
+++ 05/promise.js
@@ -71,3 +71,25 @@ async function consumePromiseFour(){
 }
 
    consumePromiseFour()
+
+
+//    async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+//    }
+//    getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
