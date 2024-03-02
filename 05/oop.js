cf8a8b3697c6add1e7602adb43154eeecd82a400
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-03-02 09:01:18 +0000

    eg 2(this keyword)

--- 05/oop.js
+++ 05/oop.js
@@ -10,6 +10,24 @@ const user = {
     }
 }
 
-console.log(user.username);
+// console.log(user.username);
 // console.log(user.getUserDetails());
 // console.log(this);
+
+
+function User(username, loginCount, isloggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isloggedIn = isloggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.user}`);
+    }
+
+    return this
+}
+
+const userOne = new User("rahul", 25, true)
+const userTwo = new User("Kumar", 58, false)
+console.log(userOne.constructor);
+// console.log(userTwo);
