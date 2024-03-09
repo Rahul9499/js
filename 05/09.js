ab492cc435f3f55cb64292bfbf0fcd5639363cd1
Rahul Anand <155736689+Rahul9499@users.noreply.github.com>
2024-03-09 10:29:51 +0000

    get set eg2

--- 05/09.js
+++ 05/09.js
@@ -1,53 +1,55 @@
-class User {
-    constructor(email, password){
-        this.email = email;
-        this.password = password
-    }
+// class User {
+//     constructor(email, password){
+//         this.email = email;
+//         this.password = password
+//     }
 
-    get email(){
-        return this._email.toUpperCase()
-    }
-    set email(value){
-     this._email = value
-    }
-    get password(){
-        return `${this._password}rahul`
-    }
+//     get email(){
+//         return this._email.toUpperCase()
+//     }
+//     set email(value){
+//      this._email = value
+//     }
+//     get password(){
+//         return `${this._password}rahul`
+//     }
 
-    set password(value){
-        this._password = value
-    }
-}
-
-const rahul = new User("rahul@gmail.ai", "abd")
-console.log(rahul.password);
-console.log(rahul.email);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function User(email, password){
-//     this.email = email;
-//     this.password = password
-
-//     Object.defineProperty(this, 'email', {
-
-//     })
+//     set password(value){
+//         this._password = value
+//     }
 // }
 
+// const rahul = new User("rahul@gmail.ai", "abd")
+// console.log(rahul.password);
+// console.log(rahul.email);
 
-// const chai = new User("chai@gmail.com", "chai")
 
-// console.log(chai.email);
+
+
+
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+      get: function(){
+    return this._email.toUpperCase()
+},
+set: function(value){
+    this._email = value
+}
+    }) 
+    Object.defineProperty(this, 'password', {
+      get: function(){
+    return this._password.toUpperCase()
+},
+set: function(value){
+    this._password = value
+}
+    }) 
+}
+
+
+const chai = new User("chai@gmail.com", "chai")
+
+console.log(chai.email);
